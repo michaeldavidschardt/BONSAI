ee9e9f1a30d3bf9398cceb9430d246ea539f1918
Michael Schardt <michaeldavidschardt@gmail.com>
2022-02-13 19:13:50 -0800

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,41 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+let secret = require('./secret');
+
+require('@nomiclabs/hardhat-waffle');
+require('@nomiclabs/hardhat-etherscan');
+require('hardhat-contract-sizer');
+
+module.exports = {
+  networks: {
+    rinkeby: {
+      url: secret.url,
+      accounts: [secret.key],
+    },
+  },
+  etherscan: {
+    apiKey: secret.eKey,
+  },
+  solidity: {
+    version: '0.8.4',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+        details: {
+          yul: true,
+          // Tuning options for the Yul optimizer.
+          yulDetails: {
+            //   // Improve allocation of stack slots for variables, can free up stack slots early.
+            //   // Activated by default if the Yul optimizer is activated.
+            stackAllocation: true,
+            //   // Select optimization steps to be applied.
+            //   // Optional, the optimizer will use the default sequence if omitted.
+            optimizerSteps: 'dhfoDgvulfnTUtnIf',
+          },
+        },
+      },
+    },
+  },
+};
